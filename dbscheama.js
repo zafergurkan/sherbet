6b0c1059d0a63dc9556b5102392977cf35615112
Zafer Gürkan <zafergurkan@outlook.com>
2020-03-16 11:11:45 +0300

    giriş yapma service yaptım

---
+++ dbscheama.js
@@ -0,0 +1,11 @@
+let db = {
+    gonderiler :[
+        {
+            userHandle: 'user',
+            body: 'Gonderi :)',
+            createdAt:'2020-03-16T06:18:19.708Z',
+            likeCount:5,
+            commentCount:2
+        }
+    ]
+}
