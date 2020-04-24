98c7ecea99b13e2dd5bb552aa5b15401383c0183
mehmet aydın <aydinmehmet.g@gmail.com>
2020-04-24 14:56:10 +0300

    kayıt olduktan sonra oturum gerçekleşti

--- sherbet-client/src/App.js
+++ sherbet-client/src/App.js
@@ -12,6 +12,8 @@ import AuthRoute from "./util/AuthRoute";
 //REDUX
 import {Provider} from 'react-redux';
 import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
 
 
 //pages
@@ -19,10 +21,10 @@ import store from './redux/store';
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
 
 const token =localStorage.FBIdToken;
 
@@ -31,11 +33,16 @@ if(token){
 
 const decodedToken = jwtDecode(token);
   if(decodedToken.exp*1000<Date.now()){
+
+      store.dispatch(logoutUser())
       window.location.href=  '/login'
-      authenticated=false;
+     
   }
   else{
-    authenticated=true;
+    
+    store.dispatch({type:SET_AUTHENTICATED});
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
 
   }
 }
@@ -50,8 +57,8 @@ class App extends Component {
                     <div className="container">
                       <Switch>
                         <Route exact path="/" component={home} />
-                        <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
-                        <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
+                        <AuthRoute exact path="/login" component={login} />
+                        <AuthRoute exact path="/signup" component={signup} />
                       </Switch>
                     </div>
                   </Router>
--- sherbet-client/src/pages/signup.js
+++ sherbet-client/src/pages/signup.js
@@ -6,9 +6,11 @@ import AppIcon from '../images/icon.png'
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import axios from 'axios';
 import Link from '@material-ui/core/Link';
 import { CircularProgress } from '@material-ui/core';
+
+import { connect } from 'react-redux';
+import { signupUser } from '../redux/actions/userActions';
 
 const styles = (theme) =>({
 
@@ -27,12 +29,24 @@ class signup extends Component {
             password: '',
             confirmPassword:'',
             handle:'',
-            loading: false,
             errors: {}
 
         }
 
     }
+
+    componentWillReceiveProps(nextProps){
+
+        if(nextProps.UI.errors){
+    
+            this.setState({
+                errors: nextProps.UI.errors
+    
+            });
+    
+        }
+    
+        }
 
     handleSubmit = (event) => {
         event.preventDefault();
@@ -47,23 +61,8 @@ class signup extends Component {
             handle:this.state.handle,
 
         }
-
-        axios.post('/signup', newUserData ).then((res) => {
-            console.log(res.data);
-            localStorage.setItem('FBIdToken',`Bearer ${res.data.token}`);
-            this.setState({
-                loading: false
-            });
-            this.props.history.push('/');
-
-        }).catch((err) => {
-            this.setState({
-                errors: err.response.data,
-                loading: false,
-
-            })
-
-        })
+        this.props.signupUser(newUserData,this.props.history);
+     
     }
 
     handleChange = (event) => {
@@ -74,8 +73,8 @@ class signup extends Component {
 
     }
     render() {
-        const { classes } = this.props;
-        const { errors, loading } = this.state;
+        const { classes , UI:{loading} } = this.props;
+        const { errors } = this.state;
         return (
             <Grid container className={classes.form}>
                 <Grid item sm />
@@ -85,7 +84,7 @@ class signup extends Component {
                         Signup
                 </Typography>
 
-                    <form noValidate onSubmit={this.hadleSumbit}>
+                    <form noValidate onSubmit={this.handleSubmit}>
                         <TextField
                             id="email"
                             name="email"
@@ -144,7 +143,7 @@ class signup extends Component {
                             variant="contained"
                             color="primary"
                             className={classes.button}
-                            onClick={this.handleSubmit}
+
                             disabled={loading}    
                             >
 
@@ -167,9 +166,18 @@ class signup extends Component {
 }
 
 signup.propTypes = {
-
-    classes: PropTypes.object.isRequired
-
-}
-
-export default withStyles(styles)(signup);
+    classes: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired,
+    signupUser: PropTypes.func.isRequired
+  };
+  
+  const mapStateToProps = (state) => ({
+    user: state.user,
+    UI: state.UI
+  });
+  
+  export default connect(
+    mapStateToProps,
+    { signupUser }
+  )(withStyles(styles)(signup));
